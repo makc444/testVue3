7e7d7c6e91b42a8fba2b9de9a6b09857288d9dec
Max_Dev <semashkinmakc@gmail.com>
2024-12-04 22:08:59 +0300

    Добавление какой-то хрени

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="app">
         <post-form @create="createPost"/>
-        <post-list :posts="posts"/>
+        <post-list :posts="posts" @remove="removePost"/>
     </div>
 </template>
 
@@ -28,7 +28,10 @@ import PostList from '@/components/PostList.vue'
       methods: {
           createPost(post) {
                 this.posts.push(post);      
-            } 
+            },
+          removePost(post) {
+              this.posts = this.posts.filter(p => p.id !== post.id)
+          } 
         },
   }
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h3>Список пользователей</h3>
-        <post-item v-for="post in posts" :post="post" :key="post.id"/>
+        <post-item v-for="post in posts" :post="post" :key="post.id" @remove = "$emit('remove', post)"/>
     </div>
 </template>
 
